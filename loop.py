c5b808e4e72e8ed6b99d4538af7b4c89052ee807
satyamgautam156-afk <satyamgautam156@gmail.com>
2026-05-22 14:47:22 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,18 +1,1 @@
 
-def calculate(num1, operator, num2):
-    if operator == '+':
-        return num1 + num2
-    elif operator == '-':
-        return num1 - num2
-    elif operator == '*':
-        return num1 * num2
-    elif operator == '/':
-        if num2 == 0:
-            return "Error: Cannot divide by zero"
-        return num1 / num2
-    else:
-        return "Error: Invalid operator"
-# --- How to use it ---
-print(calculate(10, '+', 5))  
-print(calculate(20, '*', 3)) 
-print(calculate(8, '/', 0))   
