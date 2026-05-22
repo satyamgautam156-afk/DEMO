fbb0714cb1c1b8e7b831a3f4cb8f3cf567478322
satyamgautam156-afk <satyamgautam156@gmail.com>
2026-05-22 14:45:12 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,10 +1,4 @@
-accessibility. You can choose to enable a more compact line height from the view settings menu.
 
-‎Calculator .py‎
-+18
-Lines changed: 18 additions & 0 deletions
-Original file line number	Diff line number	Diff line change
-@@ -0,0 +1,18 @@
 def calculate(num1, operator, num2):
     if operator == '+':
         return num1 + num2
